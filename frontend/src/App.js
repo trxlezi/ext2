1d0f55f978b549523f83191c9dc97645e913594f
yukiosz <rafaelyukioshira@gmail.com>
2024-10-21 10:10:38 -0300

    Correcao header e backgroundImage em missoes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <>
       <Header />
-      <Carousel />
       <Help />
       <Mission />
       <Footer />
--- frontend/src/components/Mission/index.jsx
+++ frontend/src/components/Mission/index.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-// import missionPic from "../../assets/missionPic.jpg"
+import missionPic from "../../assets/missionPic.jpg"
 import styles from "./Mission.module.css";
 
 function Mission() {
   return (
-    <section className={styles.missionSection}>
-      <div className={styles.textContainer}>
-        <h1 className={styles.title}>Missão</h1>
-        <p className={styles.description}>
-          Lutar pela defesa dos direitos animais por meio de ações ativas e
-          preventivas, que valorizem práticas sustentáveis e potencializem
-          forças para uma transformação integrada.
-        </p>
-        <button className={styles.contributeButton}>Contribuir</button>
-      </div>
-      <div className={styles.imageContainer}>
-        {/* <img src={missionPic} alt="" /> */}
-      </div>
-    </section>
+    <div style={{
+      backgroundImage: `url(${missionPic})`,
+      backgroundSize: 2200
+    }}>
+      <section className={styles.missionSection}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>Missão</h1>
+          <p className={styles.description}>
+            Lutar pela defesa dos direitos animais por meio de ações ativas e
+            preventivas, que valorizem práticas sustentáveis e potencializem
+            forças para uma transformação integrada.
+          </p>
+          <button className={styles.contributeButton}>Contribuir</button>
+        </div>
+      </section>
+    </div>
   );
 }
 
