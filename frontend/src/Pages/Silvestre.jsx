6eb188ee1d4cb70af52c96b394c604a778ae2c1a
yukiosz <rafaelyukioshira@gmail.com>
2024-10-22 21:44:38 -0300

    alteracoes no sobre (timeline)

--- frontend/src/Pages/Silvestre.jsx
+++ frontend/src/Pages/Silvestre.jsx
@@ -2,7 +2,6 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import TimeLine from "../components/TImeLine";
 
-
 function Silvestre() {
     return (
         <>
