79458e16395be209cdac178319bc9561a481ff9a
prodbyvinx <prod.vinx@gmail.com>
2024-10-23 13:33:47 -0300

    Atualizações CSS

--- frontend/src/Pages/Silvestre.jsx
+++ frontend/src/Pages/Silvestre.jsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import TimeLine from "../components/TImeLine";
+import TimeLine from "../components/TimeLine";
 
 function Silvestre() {
     return (
